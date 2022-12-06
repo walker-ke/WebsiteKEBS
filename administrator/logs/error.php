6325996c99499a0f2c0e94e39b8e4f4bc7d194d3
valeriekebz <valeriekebz@gmail.com>
2022-12-05 23:54:56 -0800

    upload files

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-11-05 06:08:31 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-11-05T06:08:31+00:00	INFO 41.212.105.225	joomlafailure	Username and password do not match or you do not have an account yet.
+2018-11-05T06:08:42+00:00	INFO 41.212.105.225	joomlafailure	Username and password do not match or you do not have an account yet.
